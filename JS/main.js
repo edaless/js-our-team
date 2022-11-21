a0b5329e6370cff37e4534ea4f8622604dc27ed8
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2022-11-21 23:21:45 +0100

    inizio

---
+++ JS/main.js
@@ -0,0 +1,33 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+
+
+
+// ciclo for console esplicativo 
+// for(let i = 0; i < arr.length; i++){
+//     console.log("arr[" + i + "]: " + arr[i]);
+
+// };
+
+// ciclo while console esplicativo 
+// let i = 0;
+// while(i < arr.length){
+//     console.log("arr[" + i + "]: " + arr[i]);
+
+//     i++;
+// };
+
+// for(){};
+
+// A = document.getElementById("B").value;
+
+
+//``
+
+// console.log("");
+// for(let i = 0; i < 10; i++){
+
+// }
+
+// }
