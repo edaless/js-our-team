5a51d62df903fabd061ba9bde7060dddbfc1ce32
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2022-11-22 19:06:55 +0100

    fine?

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,68 @@
 // Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
-// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// Ogni membro è caratterizzato dalle seguenti informazioni: 
+// nome, ruolo e foto.
+
+
+const ourTeam = [
+    {
+        "nome": ["Wayne Barnett"],
+        "ruolo": ["Founder & CEO"],
+        "foto": ["wayne-barnett-founder-ceo.jpg"]    
+    },
+    {
+        "nome": ["Angela Caroll"],
+        "ruolo": ["Chief Editor"],
+        "foto": ["angela-caroll-chief-editor.jpg"]    
+    },
+    {
+        "nome": ["Walter Gordon"],
+        "ruolo": ["Office Manager"],
+        "foto": ["walter-gordon-office-manager.jpg"]    
+    },
+    {
+        "nome": ["Angela Lopez"],
+        "ruolo": ["Social Media Manager"],
+        "foto": ["angela-lopez-social-media-manager.jpg"]    
+    },
+    {
+        "nome": ["Scott Estrada"],
+        "ruolo": ["Developer"],
+        "foto": ["scott-estrada-developer.jpg"]    
+    },
+    {
+        "nome": ["Barbara Ramos"],
+        "ruolo": ["Graphic Designer"],
+        "foto": ["barbara-ramos-graphic-designer.jpg"]    
+    }
+]
+
+const container = document.querySelector(".container");
+
+
+
+for(let i = 0; i < ourTeam.length; i++){
+
+    // indico che numero di persona è, partendo da 1
+    console.log("persona " + (i + 1));
+    container.innerHTML += `Persona ${i + 1}`;
+    for (let key in ourTeam[i]){
+        console.log(`${key}: ${ourTeam[i][key]}`);
+        container.innerHTML += `<div>${key}: ${ourTeam[i][key]}</div>`;
+    } 
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
 
 
 
